e719b9dc69a8d09ba111582fa99b084ae96d36e6
Denguiros <59782633+Denguiros@users.noreply.github.com>
2021-05-27 15:45:42 +0100

    Added files

---
+++ lib/pages/about.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart' as lottie;
+import 'package:horizontal_card_pager/horizontal_card_pager.dart';
+import 'package:horizontal_card_pager/card_item.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class AboutWidget extends StatefulWidget {
+  AboutWidget({Key? key}) : super(key: key);
+
+  @override
+  _AboutWidgetState createState() => _AboutWidgetState();
+}
+
+class _AboutWidgetState extends State<AboutWidget> {
+  int i = 0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  String name = "Semeh Chriha";
+  @override
+  Widget build(BuildContext context) {
+    List<String> names = [
+      "Fehmi Denguir",
+      "Semeh Chriha",
+      "Mohamed Habib Faouel"
+    ];
+    changeText(int i) {
+      setState(() {
+        name = names[i].toString();
+        print(name);
+      });
+    }
+
+    List<CardItem> items = [
+      ImageCarditem(
+          image: Image.asset(
+        "assets/images/Profile1.jpg",
+      )),
+      ImageCarditem(
+          image: Image.asset(
+        "assets/images/Profile1.jpg",
+      )),
+      ImageCarditem(
+          image: Image.asset(
+        "assets/images/Profile1.jpg",
+      )),
+    ];
+
+    return Scaffold(
+        backgroundColor: Colors.grey.shade200,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: TextLiquidFill(
+                  text: 'Our Team',
+                  waveColor: Colors.blueAccent,
+                  boxBackgroundColor: Theme.of(context).colorScheme.surface,
+                  textStyle: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  boxHeight: 100.0,
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 250,
+                child: lottie.Lottie.asset(
+                  "assets/lottie/38281-team.json",
+                  animate: true,
+                  repeat: true,
+                  reverse: true,
+                  alignment: Alignment.center,
+                ),
+                padding: const EdgeInsets.only(bottom: 50),
+              ),
+              Container(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: Text(
+                    '$name',
+                    style: TextStyle(
+                      shadows: <Shadow>[
+                        Shadow(
+                          offset: Offset(1.0, 1.0),
+                          blurRadius: 1.0,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
+                        Shadow(
+                          offset: Offset(1, 1.0),
+                          blurRadius: 1.0,
+                          color: Color.fromARGB(125, 0, 0, 255),
+                        ),
+                      ],
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  )),
+              HorizontalCardPager(
+                onPageChanged: (page) {
+                  print("page : $page");
+                  changeText(page.toInt());
+                },
+                onSelectedItem: (page) {
+                  print("selected : $page");
+                },
+                items: items,
+                initialPage: 1,
+              ),
+            ],
+          ),
+        ));
+  }
+}
